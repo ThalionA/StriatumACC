6a071866272e01a395e0e53f5dff4e509fee382b
Theoklitos Amvrosiadis <theoamvr@gmail.com>
2024-10-10 14:02:16 +0100

    Update reorganize_spikes_by_area.m

--- Striatum project/reorganize_spikes_by_area.m
+++ Striatum project/reorganize_spikes_by_area.m
@@ -2,12 +2,13 @@ function all_data = reorganize_spikes_by_area(all_data, ianimal)
     % Reorganizes final_spikes and final_areas so that DMS units come first, then ACC units.
 
     is_dms = strcmp(all_data(ianimal).final_areas, 'DMS');
+    is_dls = strcmp(all_data(ianimal).final_areas, 'DLS');
     is_acc = strcmp(all_data(ianimal).final_areas, 'ACC');
 
     % Reorder spikes
-    temp_final_spikes = [all_data(ianimal).final_spikes(is_dms, :); all_data(ianimal).final_spikes(is_acc, :)];
+    temp_final_spikes = [all_data(ianimal).final_spikes(is_dms, :); all_data(ianimal).final_spikes(is_dls, :); all_data(ianimal).final_spikes(is_acc, :)];
     all_data(ianimal).final_spikes = temp_final_spikes;
 
     % Reorder areas
-    all_data(ianimal).final_areas = [all_data(ianimal).final_areas(is_dms), all_data(ianimal).final_areas(is_acc)];
+    all_data(ianimal).final_areas = [all_data(ianimal).final_areas(is_dms), all_data(ianimal).final_areas(is_dls), all_data(ianimal).final_areas(is_acc)];
 end
