9344240613379eef9917d7f93902e5fe3082a373
Theoklitos Amvrosiadis <theoamvr@gmail.com>
2024-10-11 15:03:56 +0100

    Update calculate_lick_precision.m

--- Striatum project/calculate_lick_precision.m
+++ Striatum project/calculate_lick_precision.m
@@ -1,4 +1,18 @@
-function p = calculate_lick_precision(l, rz_start)
+function [p, shuffled_lick_precision_mean, shuffled_lick_precision_sem, chance_quantile] = calculate_lick_precision(l, rz_start)
 
 l(l>rz_start) = nan;
-p = sum((l-rz_start).^2, 'omitmissing');
+p = sum((l-rz_start).^2, 2, 'omitmissing');
+
+lick_number = sum(~isnan(l));
+
+shuffled_lick_precision_mean = nan;
+shuffled_lick_precision_sem = nan;
+chance_quantile = nan;
+
+if lick_number > 0
+    shuffled_l = rz_start.*rand(500, lick_number);
+    shuffled_lick_precision = sum((shuffled_l-rz_start).^2, 2);
+    shuffled_lick_precision_mean = mean(shuffled_lick_precision);
+    shuffled_lick_precision_sem = sem(shuffled_lick_precision);
+    chance_quantile = scalar_to_quantile(shuffled_lick_precision, p);
+end
